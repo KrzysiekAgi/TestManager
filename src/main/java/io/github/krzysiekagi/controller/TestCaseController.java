c7cf08fa495de6d8cddc43d52e6c6d0385ce53c5
Krzysztof Agieńczuk <krzysztof_agienczuk@live.com>
2022-05-01 20:02:53 +0200

    Drop ResponseEntity where not needed

--- src/main/java/io/github/krzysiekagi/controller/TestCaseController.java
+++ src/main/java/io/github/krzysiekagi/controller/TestCaseController.java
@@ -3,11 +3,9 @@ package io.github.krzysiekagi.controller;
 import io.github.krzysiekagi.model.TestCase;
 import io.github.krzysiekagi.service.TestCaseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
@@ -22,34 +20,34 @@ public class TestCaseController {
     }
 
     @GetMapping("/tests")
-    public ResponseEntity<List<TestCase>> getTestCases() {
-        return ResponseEntity.ok().body(testCaseService.getTests());
+    public List<TestCase> getTestCases() {
+        return testCaseService.getTests();
     }
 
     @GetMapping("/tests/{id}")
-    public ResponseEntity<TestCase> getTestCase(@PathVariable Long id) {
-        return ResponseEntity.ok().body(testCaseService.getTestById(id));
+    public TestCase getTestCase(@PathVariable Long id) {
+        return testCaseService.getTestById(id);
     }
 
     @PutMapping("/tests/{id}/status")
-    public ResponseEntity<TestCase> changeTestStatus(@PathVariable Long id, @RequestBody String status) {
-        return ResponseEntity.ok().body(testCaseService.updateStatus(id, status));
+    public TestCase changeTestStatus(@PathVariable Long id, @RequestBody String status) {
+        return testCaseService.updateStatus(id, status);
     }
 
     @PutMapping("/tests/{id}/name")
-    public ResponseEntity<TestCase> changeTestName(@PathVariable Long id, @RequestBody String name) {
-        return ResponseEntity.ok().body(testCaseService.updateTestName(id, name));
+    public TestCase changeTestName(@PathVariable Long id, @RequestBody String name) {
+        return testCaseService.updateTestName(id, name);
     }
 
     @PostMapping("/tests")
-    public ResponseEntity<?> createTestCase(@RequestBody String name) throws URISyntaxException {
-        TestCase savedTestCase = testCaseService.addTest(name);
-        return ResponseEntity.created(new URI("/tests/" + savedTestCase.getId())).body(savedTestCase);
+    @ResponseStatus(HttpStatus.CREATED)
+    public TestCase createTestCase(@RequestBody String name) {
+        return testCaseService.addTest(name);
     }
 
     @DeleteMapping("/tests/{id}")
-    public ResponseEntity<String> deleteTestCase(@PathVariable Long id) {
-        return ResponseEntity.ok().body(testCaseService.deleteTest(id));
+    public String deleteTestCase(@PathVariable Long id) {
+        return testCaseService.deleteTest(id);
     }
     
 }
